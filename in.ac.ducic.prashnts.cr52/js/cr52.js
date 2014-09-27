bcd86a9764e256f7949328ef473cf4599b9da374
Prashant Sinha <prashnts@live.com>
2014-09-28 02:47:14 +0530

    Wrapping up, for tonight.

--- in.ac.ducic.prashnts.cr52/js/cr52.js
+++ in.ac.ducic.prashnts.cr52/js/cr52.js
@@ -1,1 +1,30 @@
-alert("test");
+var Cr52 = {
+	iterationFrequency: 100,
+	updateFrequency: 2000,
+	lastUpdate: false,
+	init: function() {
+		window.setInterval(function() {
+			Cr52.routine();
+		}, Cr52.iterationFrequency);
+	},
+
+	routine: function() {
+
+	},
+	proceed: function() {
+		if (this.time.doUpdate() ||
+			)
+	},
+	time: {
+		now: function() {
+			return new Date().getTime();
+		},
+		doUpdate: function() {
+			if (!lastUpdates) return true;
+			else if (this.now() >= Cr52.updateFrequency + Cr52.lastUpdate) return true;
+			else return false;
+		}
+	}
+};
+
+Cr52.init();
