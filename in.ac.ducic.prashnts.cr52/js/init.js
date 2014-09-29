7616f3738be03bf68d566a73110925510881775a
Prashant Sinha <prashnts@live.com>
2014-09-29 09:50:18 +0530

    Moved out the Init, so that library could have different behaviours, as well.

---
+++ in.ac.ducic.prashnts.cr52/js/init.js
@@ -0,0 +1,11 @@
+/*!
+ * -.-. .-. ..... ..---
+ * Cr52 Chrome - Invokes the extension service.
+ * @author          Prashant Sinha <firstname><lastname>@outlook.com, <firstname>@ducic.ac.in
+ * @organization    Cluster Innovation Centre, University of Delhi
+ * @version         0.1
+ * @license         MIT License [opensource.org/licenses/MIT]
+ * @link            github.com/PrashntS/Cr52
+ */
+
+Cr52.init();
